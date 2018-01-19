72be5ae0e7614f70c4aab40c8c0700e97346587e
TurkeyLegs <josephrlink@gmail.com>
2018-01-18 17:06:55 -0800

    CLEAN CODE NO ERRORS

--- src/org/usfirst/frc/team5920/robot/Robot.java
+++ src/org/usfirst/frc/team5920/robot/Robot.java
@@ -15,9 +15,7 @@ import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import org.usfirst.frc.team5920.robot.commands.ExampleCommand;
 import org.usfirst.frc.team5920.robot.subsystems.DriveTrain_Subsystem;
-import org.usfirst.frc.team5920.robot.subsystems.ExampleSubsystem;
 
 /**
  * The VM is configured to automatically run this class, and to call the
