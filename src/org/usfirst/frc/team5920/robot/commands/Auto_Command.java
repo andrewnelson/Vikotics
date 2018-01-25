b700dc624d2a019f5163338756ada23375a00979
GTBlomgren <gtblomgren@gmail.com>
2018-01-24 16:09:47 -0800

    starting auto commands

--- src/org/usfirst/frc/team5920/robot/commands/Auto_Command.java
+++ src/org/usfirst/frc/team5920/robot/commands/Auto_Command.java
@@ -20,6 +20,7 @@ public class Auto_Command extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	Robot.driveTrain_Subsystem
     }
 
     // Make this return true when this Command no longer needs to run execute()
