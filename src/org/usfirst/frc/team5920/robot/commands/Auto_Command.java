c3e78450fe8c550dca27388458a41a8a489d9d78
TurkeyLegs <josephrlink@gmail.com>
2018-02-06 18:49:56 -0800

    what?

--- src/org/usfirst/frc/team5920/robot/commands/Auto_Command.java
+++ src/org/usfirst/frc/team5920/robot/commands/Auto_Command.java
@@ -2,7 +2,7 @@ package org.usfirst.frc.team5920.robot.commands;
 
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
-import org.usfirst.frc.team5920.robot.Robot;
+import org.usfirst.frc.team5920.robot.Robot; 
 
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 
