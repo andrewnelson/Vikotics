408395b9167cb4083f82a2970f0100876bc3bf87
VikoticRlink <rlink@linkdheartsranch.com>
2018-02-28 08:26:45 -0800

    Feedback Field State

--- src/org/usfirst/frc/team5920/robot/commands/Disabled_Command.java
+++ src/org/usfirst/frc/team5920/robot/commands/Disabled_Command.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team5920.robot.commands;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  *
@@ -22,6 +23,7 @@ public class Disabled_Command extends Command {
     protected void execute() {
     		String gameData;
 		gameData = DriverStation.getInstance().getGameSpecificMessage();
+		SmartDashboard.putString(  "Field State", gameData);
 		if(gameData.charAt(0) == 'L')
 		{
 			//Put left auto code here
